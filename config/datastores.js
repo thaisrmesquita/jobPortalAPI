f958816fbd8734a5b7a93bf73a3dd88a287e3205
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2020-06-23 07:06:56 -0300

    configuração da conexão com banco de dados mysql

--- config/datastores.js
+++ config/datastores.js
@@ -50,7 +50,12 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
-
+    adapter: 'sails-mysql',
+    user: 'root',
+    password: '12345678',
+    port:'3306',
+    host:'localhost',
+    database: 'portal_db'
   },
 
 
